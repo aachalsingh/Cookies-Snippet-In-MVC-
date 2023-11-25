ac4f4bba18c3ad64a8b9166f86c4dc83f6c93763
Aachal Singh <aachal.singh07@gmail.com>
2023-11-25 19:11:24 +0530

    Cookies

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using CookiesDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookiesDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User u)
+        {
+            if(ModelState.IsValid == true)
+            {
+                HttpCookie cookie = new HttpCookie("Username");
+                cookie.Value = u.Username;
+
+                //now to store in browser
+                HttpContext.Response.Cookies.Add(cookie);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            return View();
+        }
+    }
+}
